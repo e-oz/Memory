352791ac3ca4797cc96c10df57512044c925ddfe
Evgeniy Tuboltsev <ets@b1.ru>
2013-03-21 15:47:52 +0400

    travis-ci

--- test/runtests.php
+++ test/runtests.php
@@ -4,14 +4,18 @@ $Autoloader = new Jamm\Autoload\Autoloader(false);
 $Autoloader->set_modules_dir(__DIR__.'/../vendors');
 $Autoloader->register_namespace_dir('Jamm\\Memory', __DIR__.'/../');
 $Autoloader->start();
+ini_set('display_errors', 0);
 
 $Storage = new \Jamm\Memory\RedisObject();
 $Test    = new \Jamm\Memory\Tests\TestMemoryObject($Storage);
 $Test->RunTests();
+$Printer = new \Jamm\Tester\ResultsPrinter($Test->getTests());
+$Printer->printResultsLine();
 foreach ($Test->getTests() as $test_result)
 {
 	if (!$test_result->isSuccessful())
 	{
+		$Printer->printFailedTests();
 		exit(1);
 	}
 }
