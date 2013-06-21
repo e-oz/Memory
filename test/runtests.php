9c84614a2810be047a6357ca72ebc3d44e8c02e0
OZ <normandiggs@gmail.com>
2013-06-21 04:18:35 +0400

    gitmodules fixing

--- test/runtests.php
+++ test/runtests.php
@@ -1,5 +1,5 @@
 <?php
-include __DIR__.'/../vendors/Autoload/lib/Jamm/Autoload/Autoloader.php';
+include __DIR__.'/../vendors/Jamm/Autoload/Autoloader.php';
 $Autoloader = new Jamm\Autoload\Autoloader(false);
 $Autoloader->set_modules_dir(__DIR__.'/../vendors');
 $Autoloader->register_namespace_dir('Jamm\\Memory', __DIR__.'/../lib/Jamm/Memory');
